9ed48843fa75fb72b4ba79c54b6cbb5adce75cb7
brendan930 <72426773+brendan930@users.noreply.github.com>
2021-05-17 21:39:35 +0800

    New

---
+++ project6/drum.js
@@ -0,0 +1,49 @@
+var first = new Audio('piano-c_C_major.wav');
+
+var second = new Audio('piano-d_D_major 12.29.23 PM.wav');
+
+var third = new Audio('piano-e_E_major.wav');
+
+var fourth = new Audio('piano-f_F_major.wav');
+
+var fifth = new Audio('piano-g_G_major.wav');
+
+var sixth = new Audio('piano-f_F_major.wav');
+
+var seventh = new Audio('piano-g_G_major.wav');
+
+
+function firstKey() {
+    console.log("1")
+    first.play();
+}
+
+function secondKey() {
+    console.log("2")
+    second.play();
+}
+
+function thirdKey() {
+    console.log("3")
+    third.play();
+}
+
+function fourthKey() {
+    console.log("4")
+    fourth.play();
+}
+
+function fifthKey() {
+    console.log("5")
+    fifth.play();
+}
+
+function sixKey() {
+    console.log("6")
+    sixth.play();
+}
+
+function seventhKey() {
+    console.log("7")
+    seventh.play();
+}
---
+++ project6/guitar.js
@@ -0,0 +1,42 @@
+var first = new Audio('1st_String_E_64kb.m4a');
+
+var second = new Audio('2nd_String_B__64kb.m4a');
+
+var third = new Audio('3rd_String_G_64kb.m4a');
+
+var fourth = new Audio('4th_String_D_64kb.m4a');
+
+var fifth = new Audio('5th_String_A_64kb.m4a');
+
+var sixth = new Audio('6th_String_E_64kb.m4a');
+
+
+function firstString() {
+    console.log("1")
+    first.play();
+}
+
+function secondString() {
+    console.log("2")
+    second.play();
+}
+
+function thirdString() {
+    console.log("3")
+    third.play();
+}
+
+function fourthString() {
+    console.log("4")
+    fourth.play();
+}
+
+function fifthString() {
+    console.log("5")
+    fifth.play();
+}
+
+function sixthString() {
+    console.log("6")
+    sixth.play();
+}
